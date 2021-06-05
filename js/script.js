b2d8da2706dfdc082bb9b046f2cc4c780268cb0e
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-06-05 21:02:07 +0430

    add a sticky mode for navbar ...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// coding loading screen ----------> 
 window.addEventListener("load", function() {
     document.querySelector(".loader-container").classList.add("loaded");
+});
+// coding sticky navbar --------> 
+window.addEventListener("scroll", function() {
+    let sticky = document.querySelector(".navbar");
+    sticky.classList.toggle("sticky", window.scrollY > 0);
 });
