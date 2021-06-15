607402056c6ba7774d6a917143149aebda1c7926
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-06-15 03:32:28 -0700

    create and configure indicator for web . . .

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+// condign indicator
+$(document).ready(() => {
+    $(window).scroll(() => {
+        let docheight = $(document).height();
+        let winheight = $(window).height();
+        let viewport = docheight = winheight;
+        let scrollpos = $(window).scrollTop();
+        let scrollpersent = (scrollpos / viewport) * 100;
+        console.log(scrollpersent);
+
+        $(".indicator").css("width", scrollpersent + "%");
+    });
+});
