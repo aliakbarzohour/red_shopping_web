e93cb3770d5158d477ae9c774366afe5c3a0aba4
AliAkbar_Zohour <71524940+aliakbarzohour@users.noreply.github.com>
2022-01-22 11:43:31 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,17 +24,16 @@ window.addEventListener("scroll", function() {
         hour = minute * 60,
         day = hour * 24;
 
-    let birthday = "Sep 30, 2023 00:00:00",
+    let birthday = "Sep 30, 2022 00:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {
-
             let now = new Date().getTime(),
                 distance = countDown - now;
-
+          
             document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
             //do something later when date is reached
             if (distance < 0) {
